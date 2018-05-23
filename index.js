89d45777f8c4f74b77f5076f35240f397fb2844f
Conor Sullivan <conorwsullivan@gmail.com>
2018-05-22 18:00:11 -0700

    Extracting into functions

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var fs = require('fs')
 var Canvas = require('canvas');
 var parseFont = require('./parse-font');
 
-var inputText = process.argv[3];
-
 function writeLine(font, ctx, line, x, y) {
   x += 2;
   y += 2;
@@ -20,27 +18,59 @@ function writeLine(font, ctx, line, x, y) {
   }
 }
 
-parseFont(process.argv[2], (font) => {
-  var textWidth = getTextWidth(inputText, font);
-  var size = [textWidth + 4, font.spacing + 4];
-  var canvas = new Canvas(size[0], size[1]);
+function readFile(filename, callback) {
+  var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(filename),
+  });
+
+  var result = [];
+
+  lineReader.on('line', (line) => {
+    result.push(line);
+    console.log('Line from file:', line);
+  });
+
+  lineReader.on('close', () => {
+    callback(result);
+  });
+}
+
+function writeText(font, ctx, text) {
+
+}
+
+function createImage(x, y, callback) {
+  var canvas = new Canvas(x, y);
   var ctx = canvas.getContext('2d');
-
   ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, size[0], size[1]);
+  ctx.fillRect(0, 0, x, y);
 
-  writeLine(font, ctx, inputText, 0, 0);
+  callback(canvas, ctx);
+}
 
+function saveImage(canvas, name) {
   var out = fs.createWriteStream(__dirname + '/out/' +
     inputText.replace(/[^0-9a-zA-Z ]/g, '') + '.png')
     , stream = canvas.pngStream();
 
-  stream.on('data', function(chunk){
+  stream.on('data', (chunk) => {
     out.write(chunk);
   });
 
-  stream.on('end', function(){
+  stream.on('end', () => {
     console.log('saved png');
+  });
+}
+
+var inputText = process.argv[3];
+
+parseFont(process.argv[2], (font) => {
+  var textWidth = getTextWidth(inputText, font);
+  createImage(textWidth + 4, font.spacing + 4, (canvas, ctx) => {
+    writeLine(font, ctx, inputText, 0, 0);
+    var outFilename = fs.createWriteStream(__dirname + '/out/' +
+      inputText.replace(/[^0-9a-zA-Z ]/g, '') + '.png')
+    saveImage(canvas, outFilename);
   });
 });
 
