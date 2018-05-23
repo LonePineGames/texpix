ad2d73d5eab35cc33960ae69772a557cc1d2cdf7
Conor Sullivan <conorwsullivan@gmail.com>
2018-05-22 17:47:56 -0700

    Write line function

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ var parseFont = require('./parse-font');
 
 var inputText = process.argv[3];
 
+function writeLine(font, ctx, line, x, y) {
+  x += 2;
+  y += 2;
+
+  for(var i=0; i < inputText.length; i++) {
+    var sprite = font.spriteMap[inputText[i]];
+    console.log(sprite);
+    ctx.drawImage(font.image,
+      sprite.location[0], sprite.location[1],
+      sprite.size[0], sprite.size[1],
+      x, y,
+      sprite.size[0], sprite.size[1]);
+    x += sprite.size[0] + 1;
+  }
+}
+
 parseFont(process.argv[2], (font) => {
   var textWidth = getTextWidth(inputText, font);
   var size = [textWidth + 4, font.spacing + 4];
@@ -13,18 +29,7 @@ parseFont(process.argv[2], (font) => {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, size[0], size[1]);
 
-  var x = 2;
-
-  for(var i=0; i < inputText.length; i++) {
-    var sprite = font.spriteMap[inputText[i]];
-    console.log(sprite);
-    ctx.drawImage(font.image,
-      sprite.location[0], sprite.location[1],
-      sprite.size[0], sprite.size[1],
-      x, 2,
-      sprite.size[0], sprite.size[1]);
-    x += sprite.size[0] + 1;
-  }
+  writeLine(font, ctx, inputText, 0, 0);
 
   var out = fs.createWriteStream(__dirname + '/out/' +
     inputText.replace(/[^0-9a-zA-Z ]/g, '') + '.png')
@@ -37,7 +42,6 @@ parseFont(process.argv[2], (font) => {
   stream.on('end', function(){
     console.log('saved png');
   });
-
 });
 
 function getTextWidth(text, font) {
